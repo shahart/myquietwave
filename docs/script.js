396979d884fd924cc1aa011a2a46158ef7f53504
shahart <shahart@users.noreply.github.com>
2026-03-05 01:43:25 +0200

    web. Add more Daily Learning

--- docs/script.js
+++ docs/script.js
@@ -95,7 +95,7 @@ async function calc() {
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is 0-indexed
     const day = date.getDate().toString().padStart(2, '0');
     const formattedDate = `${year}-${month}-${day}`;
-    const url3 = `https://www.hebcal.com/hebcal?v=1&cfg=json&F=on&start=` + formattedDate + `&end=` + formattedDate;
+    const url3 = `https://www.hebcal.com/hebcal?v=1&cfg=json&F=on&myomi=on&nyomi=on&dty=on&start=` + formattedDate + `&end=` + formattedDate;
 
     try {
         const [resp1, resp2, resp3] = await Promise.all([
@@ -243,6 +243,10 @@ async function calc() {
         try {
             document.getElementById('dafYomi').innerHTML = resp3.items[0].hebrew;
             document.getElementById('dafYomiUrl').href = resp3.items[0].link;
+            ttip = "משנה יומית: " + resp3.items[1].hebrew + "\n" + "נ'ך יומי : " + resp3.items[2].hebrew + "\n" + "תנ'ך יומי : " + resp3.items[3].hebrew;
+            document.getElementById('dafYomi').onclick = function() {
+                alert(ttip);
+            }
         } catch (error) {
             alert("Error fetching DafYomi data " + error);
         }
